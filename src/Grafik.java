4a1c49744ef7614ca3c2c3ef8e60f64a9644736b
nuxxe0 <159939027+nuxxe0@users.noreply.github.com>
2024-04-17 07:58:58 -0700

    Unidad 3 - Actividad 25
    25.- Implementa la actualización de Graphics a través del teclado y los eventos
    
    El evento del teclado debe mover el elemento player  
    Utilizando wasd o las teclas de tu preferencia

--- src/Grafik.java
+++ src/Grafik.java
@@ -18,9 +18,15 @@ public class Grafik extends JFrame implements KeyListener{
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
+	
+	public int x =270,y=170;
+	
 	/**
 	 * Launch the application.
 	 */
+	
+	
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -50,29 +56,30 @@ public class Grafik extends JFrame implements KeyListener{
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panelArriba = new JPanel();
-		panelArriba.setBackground(Color.MAGENTA);
+		panelArriba.setBackground(Color.LIGHT_GRAY);
 		contentPane.add(panelArriba, BorderLayout.CENTER);
 		
 		
 		JPanel panelAbajo = new JPanel();
-		panelAbajo.setBackground(Color.GREEN);
+		panelAbajo.setBackground(Color.GRAY);
 		contentPane.add(panelAbajo, BorderLayout.SOUTH);
 		
 		JButton btnReiniciar = new JButton("Reiniciar");
 		btnReiniciar.setBackground(Color.WHITE);
+		btnReiniciar.setFocusable(false);
 		btnReiniciar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		panelAbajo.add(btnReiniciar);
 	}
 	
 	
-	
+	@Override
 	public void paint (Graphics g)
 	{
 		super.paint(g);
     	Graphics g2d = (Graphics)g;
     	g2d.setColor(Color.white);
-    	g2d.drawLine(30, 0, 770, 450);//linea
-    	g2d.fillRect(275, 175, 50, 50);//cuadraro
+    	//g2d.drawLine(30, 0, 770, 450);//linea
+    	g2d.fillRect(x, y, 50, 50);//cuadraro
 	}
 
 	@Override
@@ -85,11 +92,35 @@ public class Grafik extends JFrame implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("Codigo te la tecla: " + e.getKeyCode() + " Tecla Presioanda: " + e.getKeyChar());
+		
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+		switch(e.getKeyCode())
+		{
+			case 87://w arriba
+				y-=10;
+				break;
+				
+			case 65://a izquierda
+				x-=10;
+				break;
+				
+			case 83://s abajo
+				y+=10;
+				break;
+				
+			case 68://d derecha
+				x+=10;
+				break;
+				
+			default:
+				break;
+		}
+		this.update(getGraphics());
 		
 	}
 	
